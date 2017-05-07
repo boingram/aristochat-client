5662400771bdaa9b56e5a0c1507669cae8c3230f
Bo Ingram <boingram@debian>
2017-05-06 19:47:33 -0600

    Use CLI flags to set username and room

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/boingram/aristochat-client"
+	"flag"
 )
 
 func main() {
-	client, err := aristochat.NewClient("ws://localhost:4000/socket/websocket?username=user2", "test")
+	var usernamePtr, roomPtr string
+	flag.StringVar(&usernamePtr,"username", "", "The user name to chat as")
+	flag.StringVar(&roomPtr,"room", "main", "The chat room to join")
+	flag.Parse()
+
+	addr := fmt.Sprintf("ws://localhost:4000/socket/websocket?username=%s", usernamePtr)
+
+	client, err := aristochat.NewClient(addr, roomPtr)
 	err = aristochat.StartUI(client)
 	fmt.Println(fmt.Sprintf("%v", err))
 }
