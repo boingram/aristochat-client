9ce73701cc48220c080a26ee445d0cefa7415244
Bo Ingram <boingram6@gmail.com>
2017-07-15 12:25:26 -0600

    pass in server name

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,15 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"github.com/boingram/aristochat-client"
+	"strings"
 )
 
 func main() {
-	var usernamePtr, roomPtr string
+	var usernamePtr, roomPtr, serverPtr string
 	flag.StringVar(&usernamePtr, "username", "", "The user name to chat as")
 	flag.StringVar(&roomPtr, "room", "main", "The chat room to join")
+	flag.StringVar(&serverPtr, "server", "", "The aristochat server to connect to")
 	flag.Parse()
 
-	addr := fmt.Sprintf("ws://localhost:4000/socket/websocket?username=%s", usernamePtr)
+	addr := fmt.Sprintf("ws://%s/socket/websocket?username=%s", strings.Replace(serverPtr, "http://", "", -1), usernamePtr)
 
 	client, err := aristochat.NewClient(addr, roomPtr)
 	err = aristochat.StartUI(client, usernamePtr)
